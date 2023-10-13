b613e974636bc5388e95570b3f084407e06aa8e4
Osler Daniel Méndez Godoy <147261056+osler12@users.noreply.github.com>
2023-10-12 20:33:11 -0700

    Update Ejercicio_4.cs
    Entrega de trabajo

--- Ejercicio_4.cs
+++ Ejercicio_4.cs
@@ -3,3 +3,39 @@
 
 Crea un programa que genere y muestre el
 resultado factorial de un numero. */
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese un número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        long factorial = CalcularFactorial(numero);
+
+        Console.WriteLine("El factorial de " + numero + " es: " + factorial);
+
+        Console.ReadLine();
+    }
+
+    static long CalcularFactorial(int numero)
+    {
+        if (numero == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            long factorial = 1;
+
+            for (int i = 1; i <= numero; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+    }
+}
