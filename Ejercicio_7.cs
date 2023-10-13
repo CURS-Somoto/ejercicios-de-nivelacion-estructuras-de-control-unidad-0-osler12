67ea47b7b826a146c088b313c7929acbd3869797
Osler Daniel Méndez Godoy <147261056+osler12@users.noreply.github.com>
2023-10-12 20:39:47 -0700

    Update Ejercicio_7.cs
    Entrega de trabajo.

--- Ejercicio_7.cs
+++ Ejercicio_7.cs
@@ -4,3 +4,18 @@
 Imprime un conteo regresivo del 10 al 1
 utilizando un bucle for.
 */
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 10; i >= 1; i--)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.ReadLine();
+    }
+}
