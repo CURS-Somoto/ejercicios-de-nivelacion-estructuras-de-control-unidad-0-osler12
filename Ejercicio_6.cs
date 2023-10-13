ca3f0b445b475632f25cc93af2b0bc22680a860a
Osler Daniel Méndez Godoy <147261056+osler12@users.noreply.github.com>
2023-10-12 20:38:03 -0700

    Update Ejercicio_6.cs
    Entrega trabajo.

--- Ejercicio_6.cs
+++ Ejercicio_6.cs
@@ -3,3 +3,25 @@
 
 Escribe un programa que diga si un número
 es par o impar.*/
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese un número entero: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El número es par");
+        }
+        else
+        {
+            Console.WriteLine("El número es impar");
+        }
+
+        Console.ReadLine();
+    }
+}
