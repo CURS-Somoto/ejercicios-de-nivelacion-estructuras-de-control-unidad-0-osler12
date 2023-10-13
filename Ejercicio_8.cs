ed6f12bc4f44186c472c2819c3b03ac485d76f42
Osler Daniel Méndez Godoy <147261056+osler12@users.noreply.github.com>
2023-10-12 20:42:52 -0700

    Update Ejercicio_8.cs
    Entrega de trabajo.

--- Ejercicio_8.cs
+++ Ejercicio_8.cs
@@ -20,3 +20,22 @@ la siguiente secuencia de patrones:
 *****
 
 */
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+    }
+}
