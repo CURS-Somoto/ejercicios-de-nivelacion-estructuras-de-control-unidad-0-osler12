79186445f4ad3371d157201b7e9677eb7abd5d94
Osler Daniel Méndez Godoy <147261056+osler12@users.noreply.github.com>
2023-10-12 20:44:46 -0700

    Update Ejercicio_9.cs
    Entrega de trabajo.

--- Ejercicio_9.cs
+++ Ejercicio_9.cs
@@ -6,3 +6,32 @@ Dado dos números, muestra el número mayor
 utilizando una estructura if
 
 */
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese el primer número: ");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo número: ");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        if (numero1 > numero2)
+        {
+            Console.WriteLine("El número mayor es: " + numero1);
+        }
+        else if (numero2 > numero1)
+        {
+            Console.WriteLine("El número mayor es: " + numero2);
+        }
+        else
+        {
+            Console.WriteLine("Ambos números son iguales");
+        }
+
+        Console.ReadLine();
+    }
+}
